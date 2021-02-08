b951af101eb5f8735bfee6d5819aa41bf090e8d4
victor solonsky <viktor.solonskiy@zimad.com>
2021-02-08 10:52:13 +0700

    Add player's data

---
+++ Assets/Scripts/CoreLogic/Player/PlayerData.cs
@@ -0,0 +1,14 @@
+﻿namespace CoreLogic.Player
+{
+    public class PlayerData
+    {
+        public int TowerHeight;
+        public int WallHeight;
+        public int UranusMines;
+        public int EnergyMines;
+        public int SlavesMines;
+        public int Uranus;
+        public int Energy;
+        public int Slaves;
+    }
+}
---
+++ Assets/Scripts/CoreLogic/Rules/GameRules.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace CoreLogic.Rules
+{
+    [CreateAssetMenu(fileName = "GameRules", menuName = "Rules/GameRules", order = 1)]
+    public class GameRules : ScriptableObject
+    {
+        
+    }
+}
